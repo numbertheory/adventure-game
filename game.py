975f3acd109087180144382c821b3011b13155bd
JP Etcheber <jetcheber@gmail.com>
2020-03-23 21:29:18 -0700

    pixel man cometh

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, caption="Hello Pyxel")
-        pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
+        pyxel.load("assets/image_map.pyxres")
         self.position = {"x": 66, "y": 66}
         pyxel.run(self.update, self.draw)
 
@@ -16,14 +16,14 @@ class App:
         if pyxel.btnp(pyxel.KEY_LEFT, hold=1, period=1):
             self.position["x"] -= 1
         if pyxel.btnp(pyxel.KEY_UP, hold=1, period=1):
-            self.position["y"] += 1
-        if pyxel.btnp(pyxel.KEY_DOWN, hold=1, period=1):
             self.position["y"] -= 1
+        if pyxel.btnp(pyxel.KEY_DOWN, hold=1, period=1):
+            self.position["y"] += 1
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-        pyxel.blt(self.position["x"], self.position["y"], 1, 0, 0, 16, 16)
+        pyxel.text(55, 41, "Hello, Pyxel!", 8)
+        pyxel.blt(self.position["x"], self.position["y"], 0, 9, 0, 6, 7)
 
 
 App()
